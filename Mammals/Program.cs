dadd05c9f1b9f7a1554955445f85b4a5e53acf19
Jeff Cogswell <jeffcogs@gmail.com>
2022-08-22 16:08:50 -0400

    Updates

---
+++ Mammals/Program.cs
@@ -0,0 +1,65 @@
+﻿
+Dog first = new Dog() { Name = "Fido", HairColor = "Black", BarkVolume = 8, LegCount = 4 };
+//Console.WriteLine(first);
+Dog second = new Dog() { Name = "Nellie", HairColor = "Yellow", BarkVolume = 5, LegCount = 4 };
+Orangutan third = new Orangutan() { Name = "Fred", HairColor = "Orange", LegCount = 2, ThumbLength = 3 };
+//Console.WriteLine(third);
+
+List<Mammal> pets = new List<Mammal>();
+pets.Add(first);  // Added the dog Fido to the list
+pets.Add(second); // Added the dog Nellie to the list
+pets.Add(third);  // Added the orangutan Fred to the list
+
+foreach (Mammal pet in pets)
+{
+	string info = pet.ToString();  // C# will call the correct ToString for the object
+	Console.WriteLine(info);
+}
+
+//Console.WriteLine();
+//Console.WriteLine("Test method!");
+//Test(third);
+
+//static void Test(Mammal pet)
+//{
+//	string data = pet.ToString();
+//	Console.WriteLine(data);
+//}
+
+class Mammal
+{
+	public string Name;
+	public string HairColor;  // What color its hair is
+	public int LegCount;      // How many legs it walks on
+
+	public void Walk()
+	{
+		Console.WriteLine("I am walking.");
+	}
+
+	// We are overriding what's called a "virtual" function.
+	// i.e. A "virtual" function is one that you can override.
+	public override string ToString()
+	{
+		return "This is a mammal";
+	}
+}
+
+class Dog : Mammal
+{
+	public int BarkVolume;   // How loud the dog's bark is
+	public override string ToString()
+	{
+		return $"Dog named {Name}, hair {HairColor}, {LegCount} legs, barks at volume {BarkVolume}";
+	}
+}
+
+class Orangutan : Mammal
+{
+	public int ThumbLength;  // How long the thumb is
+	public override string ToString()
+	{
+		return $"Orangutan named {Name}, hair {HairColor}, {LegCount} legs, thumb is {ThumbLength} inches.";
+	}
+}
+
---
+++ Vehicles/Program.cs
@@ -0,0 +1,78 @@
+﻿Sedan mycar = new Sedan(4, "Blue", true, 4);
+mycar.Drive();
+
+RaceCar myracecar = new RaceCar(4, "Red", 400);
+myracecar.Drive();
+
+RaceCar secondracer = new RaceCar(3, "Yellow", 1000);
+
+List<Vehicle> cars = new List<Vehicle>();
+cars.Add(mycar);
+cars.Add(myracecar);
+cars.Add(secondracer);
+
+Console.WriteLine();
+Console.WriteLine("Everybody drive!");
+
+foreach (Vehicle car in cars)
+{
+	car.Drive();
+}
+
+// This commented code demonstrates polymorphism
+//Vehicle something;
+//something = mycar;
+//something.Drive(); // It will call the correct version
+
+//something = myracecar;
+//something.Drive(); // Again, correct version
+
+// Calling the correct version is known as "polymorphism".
+
+class Vehicle
+{
+	public int WheelCount;
+	public string Color;
+	public Vehicle(int _WheelCount, string _Color)
+	{
+		WheelCount = _WheelCount;
+		Color = _Color;
+	}
+	public virtual void Drive()
+	{
+		Console.WriteLine($"I am driving a {Color} car with {WheelCount} wheels.");
+	}
+}
+
+class Sedan : Vehicle  // Ignore the error about construtor parameters. We'll get there!
+{
+	public bool HasHatchBack;
+	public int DoorCount;
+	public Sedan(int _SedanWheelCount, string _SedanColor, bool _HasHatchBack, int _DoorCount) 
+		: base(_SedanWheelCount, _SedanColor)
+	{
+		WheelCount = _SedanWheelCount;
+		Color = _SedanColor;
+		HasHatchBack = _HasHatchBack;
+		DoorCount = _DoorCount;
+	}
+	public override void Drive()
+	{
+		Console.WriteLine($"I am driving speed limit in my {Color} {DoorCount}-door car!");
+	}
+}
+
+class RaceCar : Vehicle
+{
+	public int EngineSize;
+	public RaceCar(int _WheelCount, string _Color, int _EngineSize)
+		: base(_WheelCount, _Color)
+	{
+		EngineSize = _EngineSize;
+	}
+
+	public override void Drive()
+	{
+		Console.WriteLine($"I am driving really fast with my {EngineSize} engine! The car is {Color}.");
+	}
+}
